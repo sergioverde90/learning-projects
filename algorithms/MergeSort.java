29b304a20a0222b7a3dbfa9f591f28051a2ba8f3
Ssvv <sergioverde90@gmail.com>
2025-03-04 16:57:32 +0100

    Update MergeSort.java

--- algorithms/MergeSort.java
+++ algorithms/MergeSort.java
@@ -1,74 +1,62 @@
 public class MergeSort {
 
-    private static final String ANSI_RED = "\u001B[32m";
-    private static final String ANSI_RESET = "\u001B[0m";
-    
     public static void main(String[] args) {
-        int[] arr = {12, 56, 34, 2,  45, 6 , 8, 98};
-        System.out.println("============ DIVIDE ===============\n");
-        
+        final int[] arr = {9,5,2,3,8,4,1,6,7};
         mergesort(arr, 0, arr.length - 1);
+
+        System.out.println("arr = " + Arrays.stream(arr).boxed().toList());
     }
 
     private static void mergesort(int[] arr, int from, int to) {
-        // base case -> single element
-        if (from >= to) {
-            return;
-        }
+        if (from == to) return;
         
-        // split current array into two: left and right
-        var mid = (from + to) / 2;
-        System.out.println("LEFT: from = " + from + ", to = " +mid);
-        System.out.println("RIGHT: from = " + (mid + 1) + ", to = " +to);
-        mergesort(arr, from, mid);
-        mergesort(arr, mid + 1, to);
-
+        // divide
+        int mid = (from + to) / 2;
+        mergesort(arr, from, mid);            // T(n) = T(n/2)
+        mergesort(arr, mid + 1, to);    // T(n) = T(n/2)  
+        
         // conquer
-        System.out.println(ANSI_RED);
-        System.out.printf("============ CONQUER from %d to %d ===============%n" + ANSI_RED, from, to);
-        merge(arr, from, to);
-        System.out.printf("============ FINISHED CONQUER from %d to %d ===============%n", from, to);
-        System.out.println(ANSI_RESET);
+        merge(arr, from, to);                 // Θ(n) -> c*n
+        
+        // So, ignoring constants: 
+        //      2T(n/2) + Θ(n) => Θ(n log(n))
     }
 
     private static void merge(int[] arr, int from, int to) {
-        // split arr into to piles
-        final double mid = Math.floor((from + (double) to) / 2);
-        final int[] left = Arrays.copyOfRange(arr, from, (int) mid + 1);
-        final int[] right = Arrays.copyOfRange(arr, ((int) mid) + 1, to + 1);
-        System.out.println("L=" + Arrays.stream(left).boxed().toList() + ", R=" + Arrays.stream(right).boxed().toList());
-
-        int[] merged = new int[left.length + right.length];
-        int i = 0, j = 0, k = 0;
+        int mid = (int) Math.floor((float) (from + to) / 2) + 1;
+        final int[] left = Arrays.copyOfRange(arr, from, mid);
+        final int[] right = Arrays.copyOfRange(arr, mid, to + 1); // because range = [from, to)
         
-        while(i < left.length && j < right.length) {
-            if (left[i] < right[j]) {
-                merged[k] = left[i];
+        // two pointer approach
+        int i = 0, j = 0, k = from;
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) { // []
+                arr[k] = left[i];
                 i++;
             } else {
-                merged[k] = right[j];
+                arr[k] = right[j];
                 j++;
             }
             k++;
         }
         
+        // at this point the remaining array is sorted up to the end of left or right.
+        // the remaining content has to be appended
         if (i < left.length) {
-            while (i < left.length) {
-                merged[k] = left[i];
-                k++;
+            // then the right 'hand' was completed, fill it with the left content
+            while(i < left.length) {
+                arr[k] = left[i];
                 i++;
+                k++;
             }
         } else {
-            while (j < right.length) {
-                merged[k] = right[j];
-                k++;
+            // then the left 'hand' was completed, fill it with the right content
+            while(j < right.length) {
+                arr[k] = right[j];
                 j++;
+                k++;
             }
         }
-
-        System.arraycopy(merged, 0, arr, from, merged.length);
-
-        System.out.println("arr = " + Arrays.stream(arr).boxed().toList());
     }
 
 }
