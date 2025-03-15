39039f9b3893d36e9bd8ed10f4fcbf6f428038d6
Ssvv <sergioverde90@gmail.com>
2025-03-15 12:39:22 +0100

    Remove runtime analysis and point out to the blog

--- algorithms/InsertionSort.java
+++ algorithms/InsertionSort.java
@@ -8,24 +8,13 @@ public class InsertionSort {
     }
     
     private static void insertionsort(int[] arr) {
-        for (int j = 1; j < arr.length; j++) {
+        for (int j = 1; j < arr.length; j++) { // runtime analysis -> https://blog.ssvv.dev/blog/algorithms-analysis/
             int i = j - 1;
             while (i >= 0 && arr[i] > arr[i+1]) { 
                 swap(arr, i, i+1);
                 i--;
             }
-            // while loop analysis: iteration table
-            //  | # | i        |
-            //  | 0 | j-1      |
-            //  | 1 | j-2      |
-            //  | 2 | j-3      |
-            //  ...
-            //  | k | j-(k+1) |
-            // Stop condition when i = 0, so: j-(k+1) = 0 => k = j-1
         }
-        // for loop analysis: summation
-        //  $ \sum\limits_{j=2}^{n} c*(j-1) = c*\sum\limits_{j=1}^{n-1}j = c*n(n-1)/2 $
-        // So, T(n) = Θ(n^2)
     }
 
     private static void swap(int[] arr, int i, int j) {
